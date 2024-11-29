843b5baa7369310224c2b7b0d6f5622c7489ebf5
Victor_2102 <victor.hugo@localize.com.br>
2024-11-29 19:05:49 -0300

    First Commit

---
+++ WebPageWtcClub/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WebPageWtcClub
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
